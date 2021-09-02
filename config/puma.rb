ba3b4d648f1524661045d8dc9cea0af4e236ab81
Colin Rymer <colin.rymer@gmail.com>
2021-09-01 21:20:11 -0400

    feat(puma): no clustering

--- config/puma.rb
+++ config/puma.rb
@@ -1,10 +1,5 @@
-workers Integer(ENV['WEB_CONCURRENCY'] || 2)
 threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
 threads threads_count, threads_count
-
-preload_app!
-
-rackup DefaultRackup
 
 if ENV.key?('PORT')
   port ENV['PORT']
@@ -16,10 +11,4 @@ if ENV.key?('RACK_ENV')
   environment ENV['RACK_ENV']
 else
   environment 'production'
-end
-
-on_worker_boot do
-  # Worker specific setup for Rails 4.1+
-  # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
-  ActiveRecord::Base.establish_connection
 end
